aaa7bf09b9bc9602a837c93669771e776c269b36
monicajorgedev <monicajorge.dev@gmail.com>
2024-07-05 12:44:55 +0200

    ejercicio terminado

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+window.addEventListener("load",()=> {
+    const btn = document.getElementById("btn")
+    btn.addEventListener("click",()=> {
+        document.body.classList.toggle("dark")
+    })
+})
